368a2cd516a1bb6cc013a772c602d370a0d1510f
JorgeRiveraMancilla <jiriveramancilla@gmail.com>
2024-06-26 21:31:00 -0400

    feat: add components and reactive forms module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,10 @@ import { AppComponent } from './app.component';
 import { NavbarComponent } from './_components/navbar/navbar.component';
 import { FooterComponent } from './_components/footer/footer.component';
 import { MemberListComponent } from './_pages/member-list/member-list.component';
+import { AddMemberComponent } from './_pages/add-member/add-member.component';
+import { TextInputComponent } from './_components/text-input/text-input.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { EditMemberComponent } from './_pages/edit-member/edit-member.component';
 
 @NgModule({
   declarations: [
@@ -14,8 +18,11 @@ import { MemberListComponent } from './_pages/member-list/member-list.component'
     NavbarComponent,
     FooterComponent,
     MemberListComponent,
+    AddMemberComponent,
+    TextInputComponent,
+    EditMemberComponent,
   ],
-  imports: [BrowserModule, HttpClientModule, AppRoutingModule],
+  imports: [BrowserModule, HttpClientModule, AppRoutingModule, ReactiveFormsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
